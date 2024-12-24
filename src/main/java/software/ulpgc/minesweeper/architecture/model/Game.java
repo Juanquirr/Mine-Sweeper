65c767c9c596b0471c81309e29cb763b13b26373
Javier <javier.castilla101@alu.ulpgc.es>
2024-12-24 15:09:42 +0000

    Game model created.

--- src/main/java/software/ulpgc/minesweeper/architecture/model/Game.java
+++ src/main/java/software/ulpgc/minesweeper/architecture/model/Game.java
@@ -1,5 +1,44 @@
 package software.ulpgc.minesweeper.architecture.model;
 
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
-    // private final List<Interaction>
+    private final List<Interaction> interactions;
+    public final GameState gameState;
+    public final GameResult gameResult;
+
+    public Game() {
+        this.interactions = new ArrayList<>();
+        this.gameState = GameState.Unbegun;
+        this.gameResult = null;
+    }
+
+    public GameState gameState() {
+        return gameState;
+    }
+
+    public GameResult gameResult() {
+        return gameResult;
+    }
+
+    public List<Interaction> interactions() {
+        return new ArrayList<>(interactions);
+    }
+
+    public Game add(Interaction interaction) {
+        this.interactions.add(interaction);
+        return this;
+    }
+
+    public record Interaction(Cell.Position position, Instant instant) {}
+
+    public enum GameState {
+        Unbegun, Begun
+    }
+
+    public enum GameResult {
+        Won, Lost
+    }
 }
